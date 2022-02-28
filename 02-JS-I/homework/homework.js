cb7ba739af6e4fc86512d6bbf1818d051566ddab
Ariel Acosta <arielacostamedina@gmail.com>
2022-02-28 12:33:07 -0300

    My First Commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola Soy Ariel";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 999;
 
 // Crea una variable booleana:
 const nuevoBool = null;
